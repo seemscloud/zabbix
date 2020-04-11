ca9261170db089aa0017e7221671e6f7d46aeb82
Mateusz Adam Katana <9096064+theanotherwise@users.noreply.github.com>
2020-04-12 01:00:16 +0200

    Update deploy_zabbix-agentd.sh

--- deploy_zabbix-agentd.sh
+++ deploy_zabbix-agentd.sh
@@ -1,6 +1,9 @@
-useradd -m -d /opt/zabbix/agent -s /bin/bash -g zabbix zabbix-agent
+ZABBIX="agent" # agent or server
 
-cat >/opt/zabbix/agent/.bash_profile << 'EndOfMessage'
+groupadd zabbix
+useradd -m -d /opt/zabbix -s /bin/bash -g zabbix zabbix
+
+cat >/opt/zabbix/.bash_profile << 'EndOfMessage'
 # Sources
 if [ -f ~/.bashrc ] ; then
     . ~/.bashrc
@@ -14,7 +17,46 @@ umask 077
 # Exports
 EndOfMessage
  
-cat >/opt/zabbix/agent/.bashrc << 'EndOfMessage'
+cat >/opt/zabbix/.bashrc << 'EndOfMessage'
+# Sources
+ 
+# Variables
+PS1="[\[\e[35m\]\u\[\e[m\]][\l]@[\[\e[1;34m\]\h\[\e[m\]][\[\e[1;36m\]\W\[\e[m\]]# "
+HISTTIMEFORMAT="%d/%m/%Y %T "
+ 
+# Path Variables
+ 
+# Aliases
+alias ls='ls --color=auto --hide=".*"'
+alias rm='rm -iv'
+alias cp='cp -iv'
+alias mv='mv -iv'
+alias rmdir='rmdir -v'
+ 
+# Exports
+export PS1
+export HISTTIMEFORMAT
+ 
+# Path Exports
+EndOfMessage
+
+useradd -m -d /opt/zabbix/$ZABBIX_ -s /bin/bash -g zabbix zabbix-$ZABBIX
+
+cat >/opt/zabbix/$ZABBIX/.bash_profile << 'EndOfMessage'
+# Sources
+if [ -f ~/.bashrc ] ; then
+    . ~/.bashrc
+fi
+ 
+# Settings
+umask 077
+ 
+# Variables (PATH, etc.)
+ 
+# Exports
+EndOfMessage
+ 
+cat >/opt/zabbix/$ZABBIX_/.bashrc << 'EndOfMessage'
 # Sources
  
 # Variables
@@ -37,7 +79,7 @@ export HISTTIMEFORMAT
 # Path Exports
 EndOfMessage
  
-./configure --prefix=/opt/zabbix/agent --enable-agent \
+./configure --prefix=/opt/zabbix/$ZABBIX --enable-$ZABBIX \
 --with-libxml2 \
 --with-unixodbc \
 --with-net-snmp \
@@ -55,11 +97,14 @@ EndOfMessage
 make
 make install
  
-mv /opt/zabbix/agent/etc/zabbix_agentd.conf /opt/zabbix/agent/etc/zabbix_agentd.conf_BACKUP
-cat /opt/zabbix/agent/etc/zabbix_agentd.conf_BACKUP | grep "Default:" -A 1 | grep -v "Default:" | grep -v "\-\-" > /opt/zabbix/agent/etc/zabbix_agentd.conf
+mv /opt/zabbix/$ZABBIX/etc/zabbix_$ZABBIXd.conf /opt/zabbix/$ZABBIX/etc/zabbix_$ZABBIXd.conf_BACKUP
+cat /opt/zabbix/$ZABBIX_USER/etc/zabbix_$ZABBIXd.conf_BACKUP | grep "Default:" -A 1 | grep -v "Default:" | grep -v "\-\-" > /opt/zabbix/$ZABBIX/etc/zabbix_$ZABBIXd.conf
  
-mkdir -p /opt/zabbix/agent/log
-mkdir -p /opt/zabbix/agent/tmp
+mkdir -p /opt/zabbix/$ZABBIX/log
+mkdir -p /opt/zabbix/$ZABBIX/tmp
 
-find /opt/zabbix/agent -exec chmod g-rwx,o-rwx {} \;
-find /opt/zabbix/agent -exec chown -R zabbix-agent:zabbix {} \;
+find /opt/zabbix -exec chmod g-rwx,o-rwx {} \;
+find /opt/zabbix -exec chown -R zabbix:zabbix {} \;
+
+find /opt/zabbix/$ZABBIX -exec chmod g-rwx,o-rwx {} \;
+find /opt/zabbix/$ZABBIX -exec chown -R zabbix-$ZABBIXzabbix {} \;
